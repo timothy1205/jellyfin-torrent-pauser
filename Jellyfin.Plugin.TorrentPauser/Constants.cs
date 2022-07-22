5de81e0fabeb5540f7d6aefe40be8b3296fe5268
timothy1205 <timothy120500@gmail.com>
2022-07-21 18:17:24 -0600

    Added entrypoint to handle SessionManager

--- Jellyfin.Plugin.TorrentPauser/Constants.cs
+++ Jellyfin.Plugin.TorrentPauser/Constants.cs
@@ -4,6 +4,6 @@ namespace Jellyfin.Plugin.TorrentPauser
     public const string PluginName = "TorrentPauser";
     public const string PluginGuid = "eb5d7894-8eef-4b36-aa6f-5d124e828ce1";
 
-    public const string Request_Login = "auth/login"
+    public const string Request_Login = "auth/login";
   }
 }
---
+++ Jellyfin.Plugin.TorrentPauser/EntryPoint.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Jellyfin.Data.Entities;
+using Jellyfin.Data.Events;
+using MediaBrowser.Controller;
+using MediaBrowser.Controller.Authentication;
+using MediaBrowser.Controller.Dto;
+using MediaBrowser.Controller.Entities;
+using MediaBrowser.Controller.Library;
+using MediaBrowser.Controller.Plugins;
+using MediaBrowser.Controller.Session;
+using MediaBrowser.Controller.Subtitles;
+using MediaBrowser.Model.Entities;
+using Microsoft.Extensions.Logging;
+
+namespace Jellyfin.Plugin.TorrentPauser
+{
+    public class EntryPoint : IServerEntryPoint
+    {
+        private readonly ISessionManager _sessionManager;
+
+        public EntryPoint(
+            ISessionManager sessionManager
+            )
+        {
+            _sessionManager = sessionManager;
+        }
+
+        public void Dispose()
+        {
+            _sessionManager.PlaybackStart -= OnPlaybackStart;
+            _sessionManager.PlaybackStopped -= OnPlaybackStopped;
+        }
+
+        public Task RunAsync()
+        {
+            _sessionManager.PlaybackStart += OnPlaybackStart;
+            _sessionManager.PlaybackStopped += OnPlaybackStopped;
+
+            return Task.CompletedTask;
+        }
+
+        private async void OnPlaybackStart(object sender, PlaybackProgressEventArgs e)
+        {
+           // Limit
+        }
+
+        private async void OnPlaybackStopped(object sender, PlaybackStopEventArgs e)
+        {
+            // Unlimit
+        }
+    }
+}
