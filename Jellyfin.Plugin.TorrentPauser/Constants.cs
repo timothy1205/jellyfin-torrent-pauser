8d55ccf0971d69ea7733fe603046713c146b8958
timothy1205 <timothy120500@gmail.com>
2022-07-21 18:11:53 -0600

    Modified template plugin

---
+++ Jellyfin.Plugin.TorrentPauser/Constants.cs
@@ -0,0 +1,9 @@
+namespace Jellyfin.Plugin.TorrentPauser
+{
+  public static class Constants {
+    public const string PluginName = "TorrentPauser";
+    public const string PluginGuid = "eb5d7894-8eef-4b36-aa6f-5d124e828ce1";
+
+    public const string Request_Login = "auth/login"
+  }
+}
---
+++ Jellyfin.Plugin.TorrentPauser/Plugin.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Jellyfin.Plugin.TorrentPauser.Configuration;
+using MediaBrowser.Common.Configuration;
+using MediaBrowser.Common.Plugins;
+using MediaBrowser.Common.Net;
+using MediaBrowser.Model.Plugins;
+using MediaBrowser.Model.Serialization;
+
+namespace Jellyfin.Plugin.TorrentPauser
+{
+    public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
+    {
+        IHttpClientFactory _httpClientFactory;
+        public Plugin(
+            IApplicationPaths applicationPaths,
+            IXmlSerializer xmlSerializer,
+            IHttpClientFactory httpClientFactory)
+            : base(applicationPaths, xmlSerializer)
+        {
+            Instance = this;
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public HttpClient GetHttpClient() {
+            var httpClient = _httpClientFactory.CreateClient(NamedClient.Default);
+            httpClient.DefaultRequestHeaders.UserAgent.Add(
+                new ProductInfoHeaderValue(Name, Version.ToString()));
+
+            return httpClient;
+        }
+
+        public override string Name => Constants.PluginName;
+
+        public override Guid Id => Guid.Parse(Constants.PluginGuid);
+
+        public static Plugin Instance { get; private set; }
+
+        public IEnumerable<PluginPageInfo> GetPages()
+        {
+            return new[]
+            {
+                new PluginPageInfo
+                { 
+                    Name = this.Name,
+                    EmbeddedResourcePath = string.Format("{0}.Configuration.configPage.html", GetType().Namespace)
+                }
+            };
+        }
+    }
+}
